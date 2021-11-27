f97b169f7386d4c37100ca6a652e29d1f73735c1
joel1842 <joeldaniellake@gmail.com>
2021-11-27 17:39:40 -0330

    Connected to Heroku database
    Queries data from Heroku server, does not display in view yet.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const { restart } = require('nodemon');
 const app = express();
+const { Client } = require('pg');
+const connectionString = 'postgres://uqnbftpplrfbme:697df29de3e98c8869fc16dba2964405f44714fff0d3bc9b53041c4bd433fb87@ec2-44-199-49-128.compute-1.amazonaws.com:5432/davgmak2f2f1ig'
+
+const client = new Client({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+client.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+})
 
 app.use(express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/css'));
@@ -9,10 +22,18 @@ app.use('/img', express.static(__dirname + '/img'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+app.listen(3001);
 
 app.get('/', (req, res) => {
-    res.render('index');
+
+    client.query('SELECT * FROM Goldfish;', (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+            console.log(res.rows[0].name)
+            console.log(res.rows[0])
+        }
+    })
 })
 
 app.get('/browse', (req, res) => {
@@ -30,3 +51,4 @@ app.get('/compatibility', (req, res) => {
 app.get('/tank', (req, res) => {
     res.render('tank');
 })
+
