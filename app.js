590cad72f5585960f4f4685ef2c086de6d0e4446
joel1842 <joeldaniellake@gmail.com>
2021-12-09 14:05:48 -0330

    deploy heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { Client } = require('pg');
+const e = require('express');
 require('dotenv').config()
 const connectionString = process.env.connectionString;
 
@@ -36,54 +37,62 @@ app.use((req, res, next) => {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
 
-  const getFishData = () => {
-      return new Promise((resolve, reject) => {
-        client.query('SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM goldfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM catfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM gourami UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM pufferfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM cyprinids UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM loaches UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM characidae', (err, res) => {
-            if (err) {
-              reject(new Error("Error!"))
-            } else {
-                resolve(res.rows);
-            }
-        })
-      })
-  }
-
-  const getTetraData = () => {
-    return new Promise((resolve, reject) => {
-        client.query('SELECT * FROM characidae', (err, res) => {
-            if (err) {
-              reject(new Error("Error!"))
-            } else {
-                resolve(res.rows);
-            }
-        })
-      })
-  }
-
-  app.get('/goldfish', (req, res) => {
-    getFishData().then((results) => {
-        res.json(results)
-    }).catch((err) => {
-        console.log("Promise rejection error: "+err);
+app.get('/goldfish', (req, res) => {
+    client.query('SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM goldfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM catfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM gourami UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM pufferfish UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM cyprinids UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM loaches UNION SELECT pic1, name, "scientificName","sizeCM", "careLevel", "plants" FROM characidae', (err, response) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(response.rows)
+        }
     })
 })
 
 app.get('/tetra', (req, res) => {
-    getTetraData().then((results) => {
-        res.json(results)
-    }).catch((err) => {
-        console.log("Promise rejection error: "+err);
+    client.query('SELECT * FROM characidae', (err, response) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(response.rows)
+        }
     })
 })
 
 app.post('/favorites', (req, res) => {
-    client.query('INSERT INTO "favoritesList" ("userID", "pic1", "fishName") VALUES ($1, $2, $3)', [req.body.user, req.body.pic, req.body.name], (err, res) => {
+
+    let user = req.body.user;
+    let fishName = req.body.name;
+    let arrayLength;
+    let verified = 0;
+
+    client.query('SELECT * FROM "favoritesList"', (err, response) => {
+
         if (err) {
             console.log(err);
         } else {
-            console.log(res);
+            let data = response;
+
+            arrayLength = data.rows.length
+
+            data.rows.forEach((row) => {
+                if (row.userID === user && row.fishName === fishName) {
+                    console.log('duplicate!')
+                } else {
+                    verified++
+                }
+            })
+        }
+
+        if (verified === arrayLength) {
+            client.query('INSERT INTO "favoritesList" ("userID", "pic1", "fishName") VALUES ($1, $2, $3);', [req.body.user, req.body.pic, req.body.name], (err, res) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('New Favorite!')
+                    console.log(res);
+                }
+            })
         }
     })
 }) 
@@ -91,7 +100,6 @@ app.post('/favorites', (req, res) => {
 app.post('/favlist', (req, res) => {
 
     const user = req.body.user
-    console.log(user)
     let favFish = []
 
     client.query('SELECT * FROM "favoritesList"', (err, response) => {
